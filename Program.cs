b8e140f888aadca8a235e7768e824cc23909cce3
Bob Heinbokel <ROBERTHEINBOKEL@GMAIL.COM>
2024-02-01 20:12:11 -0500

    added a readme and a person class

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GitPractice.models;
+
+Person person = new Person();
+
+person.FirstName = "Bob";
+person.LastName = "Heinbokel";
+
+Console.WriteLine(person.FirstName + ' ' + person.LastName);
